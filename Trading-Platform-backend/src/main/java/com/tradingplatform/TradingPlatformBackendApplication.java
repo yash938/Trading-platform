3cdaaf083e92d8c7d2dbf2d3aa859cd1d7105adf
Yash Sharma <yash32860@gmail.com>
2024-12-22 15:01:31 +0530

    first commit

---
+++ Trading-Platform-backend/src/main/java/com/tradingplatform/TradingPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tradingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingPlatformBackendApplication.class, args);
+	}
+
+}
